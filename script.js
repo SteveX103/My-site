0e632825d2edcc89fed7cbd9ff0a7be27613f4e4
Pratyush Biswas <pratyushbiswas103@gmail.com>
2026-01-09 09:52:11 +0530

    V2

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const reveals = document.querySelectorAll(".reveal");
 
 window.addEventListener("scroll", () => {
@@ -10,3 +11,17 @@ window.addEventListener("scroll", () => {
     }
   });
 });
+=======
+const reveals = document.querySelectorAll(".reveal");
+
+window.addEventListener("scroll", () => {
+  reveals.forEach(section => {
+    const sectionTop = section.getBoundingClientRect().top;
+    const trigger = window.innerHeight - 100;
+
+    if (sectionTop < trigger) {
+      section.classList.add("active");
+    }
+  });
+});
+>>>>>>> 0ea9872 (V2)
